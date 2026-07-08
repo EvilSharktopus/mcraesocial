3bb457dd2506e44a4e52d83797590184fc77aaaa
EvilSharktopus <adam.j.mcrae@gmail.com>
2026-07-08 12:30:38 -0600

    fix: remove basename /gravity from BrowserRouter

--- political-gravity/src/App.jsx
+++ political-gravity/src/App.jsx
@@ -28,7 +28,7 @@ function RootRedirect() {
 export default function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter basename="/gravity">
+      <BrowserRouter>
         <AuthProvider>
           <Routes>
             <Route path="/login" element={<Login />} />
